89c8184ce61ead9377ce7384d81f4bf25a171892
alexcao <alexcao194@gmail.com>
2024-04-03 19:44:50 +0700

    ok

--- lib/app/presentation/screen/home/home_screen.dart
+++ lib/app/presentation/screen/home/home_screen.dart
@@ -1,22 +1,19 @@
+import 'package:flashy_tab_bar2/flashy_tab_bar2.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:molten_navigationbar_flutter/molten_navigationbar_flutter.dart';
 import 'package:trapper/app/presentation/bloc/profile/profile_bloc.dart';
 import 'package:trapper/app/presentation/screen/home/home_tabs/widget/keep_alive_page.dart';
-import 'package:trapper/app/presentation/screen/rooms/widget/header_message.dart';
-import 'package:trapper/app/presentation/screen/rooms/widget/room_avatar.dart';
 import 'package:trapper/config/go_router/app_go_router.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../../generated/l10n.dart';
 
-import '../../../../config/const/dimen.dart';
 import '../../../domain/entity/profile.dart';
 import 'home_tabs/connect_tab.dart';
 import 'home_tabs/friends_tab.dart';
 import 'home_tabs/help_tab.dart';
 import 'home_tabs/profile_tab.dart';
 import 'home_tabs/settings_tab.dart';
-import 'home_tabs/widget/profile_button.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -51,78 +48,66 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return SafeArea(
-      child: BlocBuilder<ProfileBloc, ProfileState>(
-        builder: (context, state) {
-          var profile = const Profile();
-          if (state is ProfileGot) {
-            profile = state.profile;
-          }
-          return Scaffold(
-            bottomNavigationBar: MoltenBottomNavigationBar(
-              selectedIndex: index,
-              onTabChange: _go,
-              margin: const EdgeInsets.only(bottom: 12, top: 4),
-              tabs: [
-                MoltenTab(
-                  icon: const Icon(Icons.person),
-                  selectedColor: Theme.of(context).colorScheme.primary,
-                  unselectedColor: Theme.of(context).colorScheme.onSurface,
-                  title: Text(S.current.profile_button),
+      child: Scaffold(
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              context.go(RoutePath.messages);
+            },
+            child: const Icon(Icons.message),
+          ),
+          backgroundColor: Theme.of(context).colorScheme.surfaceVariant,
+          body: Row(
+            children: [
+              RotatedBox(
+                quarterTurns: 5,
+                child: FlashyTabBar(
+                  selectedIndex: index,
+                  onItemSelected: _go,
+                  items: [
+                    FlashyTabBarItem(
+                      icon: const Icon(Icons.person),
+                      title: Text(S.current.profile_button),
+                    ),
+                    FlashyTabBarItem(
+                      icon: const Icon(Icons.people),
+                      title: Text(S.current.friends_button),
+                    ),
+                    FlashyTabBarItem(
+                      icon: const Icon(Icons.connect_without_contact),
+                      title: Text(S.current.connect_button),
+                    ),
+                    FlashyTabBarItem(
+                      icon: const Icon(Icons.settings),
+                      title: Text(S.current.settings_button),
+                    ),
+                    FlashyTabBarItem(
+                      icon: const Icon(Icons.help),
+                      title: Text(S.current.help_button),
+                    ),
+                  ],
                 ),
-                MoltenTab(
-                  icon: const Icon(Icons.people),
-                  selectedColor: Theme.of(context).colorScheme.primary,
-                  unselectedColor: Theme.of(context).colorScheme.onSurface,
-                  title: Text(S.current.friends_button),
-                ),
-                MoltenTab(
-                  icon: const Icon(Icons.connect_without_contact),
-                  selectedColor: Theme.of(context).colorScheme.primary,
-                  unselectedColor: Theme.of(context).colorScheme.onSurface,
-                  title: Text(S.current.connect_button),
-                ),
-                MoltenTab(
-                  icon: const Icon(Icons.settings),
-                  selectedColor: Theme.of(context).colorScheme.primary,
-                  unselectedColor: Theme.of(context).colorScheme.onSurface,
-                  title: Text(S.current.settings_button),
-                ),
-                MoltenTab(
-                  icon: const Icon(Icons.help),
-                  selectedColor: Theme.of(context).colorScheme.primary,
-                  unselectedColor: Theme.of(context).colorScheme.onSurface,
-                  title: Text(S.current.help_button),
-                ),
-              ],
-            ),
-            floatingActionButton: FloatingActionButton(
-              onPressed: () {
-                context.go(RoutePath.messages);
-              },
-              child: const Icon(Icons.message),
-            ),
-            backgroundColor: Theme.of(context).colorScheme.surfaceVariant,
-            body: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.surface,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: PageView(
-                    controller: _pageController,
-                    physics: const NeverScrollableScrollPhysics(),
-                    children: const [
-                      KeepAlivePage(child: Center(child: ProfileTab())),
-                      KeepAlivePage(child: Center(child: FriendsTab())),
-                      KeepAlivePage(child: Center(child: ConnectTab())),
-                      KeepAlivePage(child: Center(child: SettingsTab())),
-                      KeepAlivePage(child: Center(child: HelpTab())),
-                    ],
-                  )),
-            )
-          );
-        },
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.surface,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: PageView(
+                      controller: _pageController,
+                      physics: const NeverScrollableScrollPhysics(),
+                      children: const [
+                        KeepAlivePage(child: Center(child: ProfileTab())),
+                        KeepAlivePage(child: Center(child: FriendsTab())),
+                        KeepAlivePage(child: Center(child: ConnectTab())),
+                        KeepAlivePage(child: Center(child: SettingsTab())),
+                        KeepAlivePage(child: Center(child: HelpTab())),
+                      ],
+                    )),
+              ),
+            ],
+          )
       ),
     );
   }
