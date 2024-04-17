3de48d40d49fcc44744daded8a6c67bea1191a19
Cao Việt Đức <alexcao194@gmail.com>
2024-04-17 09:04:06 -0700

    text width limit

--- lib/app/presentation/screen/rooms/widget/text_message.dart
+++ lib/app/presentation/screen/rooms/widget/text_message.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../config/const/dimen.dart';
+
 class TextMessage extends StatelessWidget {
   const TextMessage({super.key, required this.isSender, required this.message});
 
@@ -8,25 +10,31 @@ class TextMessage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Padding(
       padding: const EdgeInsets.all(4.0),
       child: Row(
         children: [
           if (isSender) Expanded(child: Container()),
-          Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 16,
-              vertical: 8,
+          ConstrainedBox(
+            constraints: BoxConstraints(
+              maxWidth: (size.width > Dimen.mobileWidth) ? size.width * 0.4 : size.width * 0.6,
             ),
-            decoration: BoxDecoration(
-              color: isSender ? Theme.of(context).colorScheme.primary : Theme.of(context).colorScheme.background,
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: Text(
-              message,
-              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                    color: isSender ? Theme.of(context).colorScheme.background : Theme.of(context).colorScheme.onBackground,
-                  ),
+            child: Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 8,
+              ),
+              decoration: BoxDecoration(
+                color: isSender ? Theme.of(context).colorScheme.primary : Theme.of(context).colorScheme.background,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Text(
+                message,
+                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      color: isSender ? Theme.of(context).colorScheme.background : Theme.of(context).colorScheme.onBackground,
+                    ),
+              ),
             ),
           ),
         ],
