dc54baa199a13846dbff27e9a153e07205d03898
Cao Việt Đức <alexcao194@gmail.com>
2024-05-09 20:23:04 -0700

    change splash and banner

--- lib/app/presentation/screen/widgets/side_banner.dart
+++ lib/app/presentation/screen/widgets/side_banner.dart
@@ -16,12 +16,15 @@ class SlideBanner extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        ConstrainedBox(
-            constraints: const BoxConstraints(
-              maxHeight: 500,
-              minHeight: 200,
-            ),
-            child: Image.asset(Assets.assetsBanner, fit: BoxFit.cover, height: size.height * 0.5)
+        Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: ConstrainedBox(
+              constraints: const BoxConstraints(
+                maxHeight: 500,
+                minHeight: 200,
+              ),
+              child: Image.asset(Assets.assetsBanner, fit: BoxFit.cover, height: size.height * 0.5)
+          ),
         ),
         Text(
           S.current.app_name,
