eb5710a74c3e1a88db9224b7656d7f987cda0f87
Cao Việt Đức <alexcao194@gmail.com>
2024-05-09 21:36:17 -0700

    feat: about tab

--- lib/app/presentation/screen/home/home_tabs/about_tab.dart
+++ lib/app/presentation/screen/home/home_tabs/about_tab.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../../generated/assets.dart';
 import '../../../bloc/auth/auth_bloc.dart';
 
 class AboutTab extends StatelessWidget {
@@ -9,12 +10,35 @@ class AboutTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: TextButton(
-        onPressed: () {
-          context.read<AuthBloc>().add(const AuthEventLogout());
-        },
-        child: const Text("Logout"),
-      )
-    );
+        child: SingleChildScrollView(
+          child: Column(
+                children: [
+          Text("QR code", style: Theme.of(context).textTheme.titleLarge),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12), color: Colors.white),
+              child: Image.asset(Assets.assetsQr),
+            ),
+          ),
+          Text(
+            "Clean Architecture",
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12), color: Colors.white),
+            child: Image.asset(Assets.assetsCa),
+          ),
+          TextButton(
+            onPressed: () {
+              context.read<AuthBloc>().add(const AuthEventLogout());
+            },
+            child: const Text("Logout"),
+          ),
+                ],
+              ),
+        ));
   }
 }
